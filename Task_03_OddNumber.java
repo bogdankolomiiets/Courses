e84b7057613dc7a95919636f89849c1356efc55d
Bogdan <bogdan.kolomiiets@gmail.com>
2019-04-12 21:55:04 +0300

    EPAM_TASKS

--- Task_03_OddNumber.java
+++ Task_03_OddNumber.java
@@ -2,14 +2,14 @@ package com.bogdan.kolomiiets.tasks;
 
 public class Task_03_OddNumber {
     public static void main(String[] args) {
-        int[] array = {49, 10, 20, 49, 30, 50, 44, 23};
+        int[] array = {49, 10, 20, 49, 30, 50, 44, 51, 23};
         showMaxOdd(array);
     }
 
     private static void showMaxOdd(int[] arr){
         int maxOdd = 0;
         for (int i = 0; i < arr.length; i++) {
-            maxOdd = arr[i] % 2 != 0 ? maxOdd > arr[i] ? maxOdd : arr[i] : 0;
+            maxOdd = arr[i] % 2 != 0 ? maxOdd > arr[i] ? maxOdd : arr[i] : maxOdd;
         }
 
         switch (maxOdd){
